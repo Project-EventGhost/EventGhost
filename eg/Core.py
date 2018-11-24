be38ebe038863610619eb8f97e9362c23f27b9b8
topic2k <topic2k+git@gmail.com>
2018-11-24 19:47:06 +0100

    some more changes

--- eg/Core.py
+++ eg/Core.py
@@ -118,7 +118,7 @@ eg.dummyAsyncoreDispatcher = None
 
 from eg.WinApi.Dynamic import GetCurrentProcessId  # NOQA
 eg.processId = GetCurrentProcessId()
-Init.InitPil()
+
 
 class Exception(Exception):
     def __unicode__(self):
